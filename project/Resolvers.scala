7d3e3bded5788ee07ee8032879941a3e9f936084
feroshjacob <feroshjacob@gmail.com>
2015-10-16 15:21:36 -0400

    This is the only repository actually required

--- project/Resolvers.scala
+++ project/Resolvers.scala
@@ -2,12 +2,12 @@
 import sbt._
 
 object Resolvers {
-  val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+ val mvnrepository = "MVN Repo" at "http://central.maven.org/maven2/"
+ /* val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   val sonatype = "Sonatype Release" at "https://oss.sonatype.org/content/repositories/releases"
-  val mvnrepository = "MVN Repo" at "http://mvnrepository.com/artifact"
   val conjars = "Concurrent Maven Repo" at "http://conjars.org/repo"
   val cloudera = "cloudera" at "https://repository.cloudera.com/artifactory/cloudera-repos/"
-
-  val allResolvers = Seq(typesafe, sonatype, mvnrepository, conjars, cloudera)
+*/
+  val allResolvers = Seq(mvnrepository)
 
 }
