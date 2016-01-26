40a23fd3bd6a2b90a4945ed1d4f0107204f62257
feroshjacob <feroshjacob@gmail.com>
2016-01-26 11:35:06 -0500

    removed the  old unZip method

--- electricexamples/src/main/scala/com/recipegrace/biglibrary/electricexamples/LanguageDetect.scala
+++ electricexamples/src/main/scala/com/recipegrace/biglibrary/electricexamples/LanguageDetect.scala
@@ -2,6 +2,7 @@ package com.recipegrace.biglibrary.electricexamples
 
 import com.cybozu.labs.langdetect.DetectorFactory
 import com.recipegrace.biglibrary.core.CreateTemporaryFiles
+import com.recipegrace.biglibrary.core.ZipArchive
 import com.recipegrace.biglibrary.electric.ElectricContext
 import com.recipegrace.biglibrary.electric.jobs.TwoInputJob
 import org.apache.spark.SparkFiles
@@ -9,7 +10,7 @@ import org.apache.spark.SparkFiles
 /**
   * Created by Ferosh Jacob on 10/30/15.
   */
-object LanguageDetect extends TwoInputJob with CreateTemporaryFiles  {
+object LanguageDetect extends TwoInputJob with CreateTemporaryFiles with ZipArchive  {
 
 
   override def execute(one: String, two: String, output: String)(implicit ec: ElectricContext): Unit = {
@@ -25,7 +26,7 @@ object LanguageDetect extends TwoInputJob with CreateTemporaryFiles  {
         val lang =
           try {
             if (DetectorFactory.getLangList.size() < 1) {
-              val profileFolder = unZipFile(SparkFiles.get("profiles.zip"))
+              val profileFolder = unZip(SparkFiles.get("profiles.zip"))
               DetectorFactory.loadProfile(profileFolder)
             }
             val detector = DetectorFactory.create()
