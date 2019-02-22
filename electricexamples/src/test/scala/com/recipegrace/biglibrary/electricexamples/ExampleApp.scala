06c4edb979b686de4d782625829de238a3950c32
feroshjacob <feroshjacob>
2019-02-22 17:26:23 -0500

    updated with electriexamples

--- electricexamples/src/test/scala/com/recipegrace/biglibrary/electricexamples/ExampleApp.scala
+++ electricexamples/src/test/scala/com/recipegrace/biglibrary/electricexamples/ExampleApp.scala
@@ -1,9 +1,8 @@
 package com.recipegrace.biglibrary.electricexamples
 
 import com.recipegrace.biglibrary.core.CreateTemporaryFiles
-import com.typesafe.scalalogging.slf4j.Logger
+
 import info.debatty.java.stringsimilarity._
-import org.slf4j.LoggerFactory
 
 /**
   * Created by Ferosh Jacob on 10/30/15.
@@ -15,26 +14,24 @@ object ExampleApp extends App with CreateTemporaryFiles {
   val f2 = "hello"
 
   val matcher1 = new NormalizedLevenshtein()
- logger.info( matcher1.distance(f1, f2)+"")
+  logger.info(matcher1.distance(f1, f2) + "")
 
   val matcher2 = new JaroWinkler()
-  logger.info(matcher2.distance(f1, f2)+"")
+  logger.info(matcher2.distance(f1, f2) + "")
 
   val matcher3 = new MetricLCS()
- logger.info(matcher3.distance(f1, f2)+"")
+  logger.info(matcher3.distance(f1, f2) + "")
 
   val matcher4 = new NGram()
- logger.info(matcher4.distance(f1, f2)+"")
+  logger.info(matcher4.distance(f1, f2) + "")
 
   val matcher5 = new Cosine()
- logger.info(matcher5.distance(f1, f2)+"")
+  logger.info(matcher5.distance(f1, f2) + "")
 
   val matcher6 = new Jaccard()
- logger.info(matcher6.distance(f1, f2)+"")
-
+  logger.info(matcher6.distance(f1, f2) + "")
 
   val matcher7 = new SorensenDice()
- logger.info(matcher7.distance(f1, f2)+"")
-
+  logger.info(matcher7.distance(f1, f2) + "")
 
 }
